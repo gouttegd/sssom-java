d3cc5befa6a9d485eca78c0c573dd6f2e08f286d
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2023-07-17 21:35:54 +0100

    Re-implement the cross-species bridge generator.
    Update the implementation of CrossSpeciesBridgeGenerator to use the
    newly defined IMappingFilter and IMappingTransformer interfaces.

--- src/main/java/org/incenp/obofoundry/sssom/CrossSpeciesBridgeGenerator.java
+++ src/main/java/org/incenp/obofoundry/sssom/CrossSpeciesBridgeGenerator.java
@@ -18,18 +18,17 @@
 
 package org.incenp.obofoundry.sssom;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.incenp.obofoundry.sssom.model.Mapping;
 import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 
 /**
  * Generate bridging axioms for cross-species mappings.
@@ -40,61 +39,42 @@ public class CrossSpeciesBridgeGenerator {
     public static String CROSS_SPECIES_BROAD_MATCH = "https://w3id.org/semapv/vocab/crossSpeciesBroadMatch";
     public static String CROSS_SPECIES_NARROW_MATCH = "https://w3id.org/semapv/vocab/crossSpeciesNarrowMatch";
     public static String CROSS_SPECIES_RELATED_MATCH = "https://w3id.org/semapv/vocab/crossSpeciesRelatedMatch";
-    public static String OBOFOUNDRY_UNIQUE_LABEL = "http://purl.obolibrary.org/obo/IAO_0000589";
 
     private static final IRI partOfIRI = IRI.create("http://purl.obolibrary.org/obo/BFO_0000050");
 
-    private OWLOntology ontology;
-    private OWLDataFactory factory;
-    private OWLClassExpression partOfTaxon;
-    private String taxonName;
+    IMappingFilter predicateFilter;
+    List<IMappingTransformer<OWLAxiom>> axiomGenerators;
 
     public CrossSpeciesBridgeGenerator(OWLOntology ontology, IRI taxonIRI) {
         this(ontology, taxonIRI, null);
     }
 
     public CrossSpeciesBridgeGenerator(OWLOntology ontology, IRI taxonIRI, String taxonName) {
-        this.ontology = ontology;
-        this.factory = ontology.getOWLOntologyManager().getOWLDataFactory();
-        this.taxonName = taxonName;
+        OWLDataFactory factory = ontology.getOWLOntologyManager().getOWLDataFactory();
 
-        partOfTaxon = factory.getOWLObjectSomeValuesFrom(factory.getOWLObjectProperty(partOfIRI),
+        OWLClassExpression partOfTaxon = factory.getOWLObjectSomeValuesFrom(factory.getOWLObjectProperty(partOfIRI),
                 factory.getOWLClass(taxonIRI));
+        
+        predicateFilter = (mapping) -> mapping.getPredicateId().equals(CROSS_SPECIES_EXACT_MATCH);
+        axiomGenerators = new ArrayList<IMappingTransformer<OWLAxiom>>();
+        axiomGenerators.add(new EquivalentAxiomGenerator(ontology, partOfTaxon));
+        if ( taxonName != null ) {
+            axiomGenerators.add(new UniqueLabelGenerator(ontology, String.format("%%s (%s)", taxonName)));
+        }
     }
 
     public Set<OWLAxiom> generateAxioms(Mapping mapping) {
-        OWLClass subject = factory.getOWLClass(IRI.create(mapping.getSubjectId()));
-        OWLClass object = factory.getOWLClass(IRI.create(mapping.getObjectId()));
-
         Set<OWLAxiom> axioms = new HashSet<OWLAxiom>();
 
-        if ( mapping.getPredicateId().equals(CROSS_SPECIES_EXACT_MATCH) ) {
-            OWLClassExpression oAndPartOfTaxon = factory.getOWLObjectIntersectionOf(object, partOfTaxon);
-            axioms.add(factory.getOWLEquivalentClassesAxiom(subject, oAndPartOfTaxon));
-
-            if ( taxonName != null ) {
-                OWLAxiom uniqueLabelAxiom = generateOBOUniqueLabelAnnotation(subject.getIRI());
-                if ( uniqueLabelAxiom != null ) {
-                    axioms.add(uniqueLabelAxiom);
+        if ( predicateFilter.filter(mapping) ) {
+            for ( IMappingTransformer<OWLAxiom> t : axiomGenerators ) {
+                OWLAxiom ax = t.transform(mapping);
+                if ( ax != null ) {
+                    axioms.add(ax);
                 }
             }
         }
 
-        // TODO: Implement other types of mapping
         return axioms;
-    }
-
-    private OWLAxiom generateOBOUniqueLabelAnnotation(IRI subject) {
-        for ( OWLAnnotationAssertionAxiom ax : ontology.getAnnotationAssertionAxioms(subject) ) {
-            if ( ax.getProperty().getIRI().equals(OWLRDFVocabulary.RDFS_LABEL.getIRI()) ) {
-                String label = ax.getValue().asLiteral().get().getLiteral();
-
-                return factory.getOWLAnnotationAssertionAxiom(
-                        factory.getOWLAnnotationProperty(IRI.create(OBOFOUNDRY_UNIQUE_LABEL)), subject,
-                        factory.getOWLLiteral(String.format("%s (%s)", label, taxonName)));
-            }
-        }
-
-        return null;
     }
 }
