gouttegd/sssom-java

commit 0ebbd0bc5e38c17ceacb5b65d603617f8b3c2a36

Allow more characers in a IRI.
IRIs may contain '?' and '&' characters, so we must allow them.

core/src/main/antlr4/org/incenp/obofoundry/sssom/transform/parser/SSSOMTransform.g4
@@ -137,7 +137,7 @@ CURIE     : PREFIX [a-zA-Z0-9_*-]+;
 
 PREFIX    : [a-zA-Z0-9_]+ ':';
 
-IRI       : '<' [a-zA-Z0-9_/:#=.-]+ '>';
+IRI       : '<' [a-zA-Z0-9_/:#=.-?&]+ '>';
 
 DOUBLE    : [0-9]+ '.' [0-9]+;
 

commit 813bde02397e8aa34103510da0312e80a73b57a0

Fix IRI recognition pattern.
This fixes a bug introduced in 0ebbd0bc, where the regular expression
pattern for IRI was modified in such a way that the '-' was no longer
the *last* character in its group, thereby changing this meaning. Doh!

core/src/main/antlr4/org/incenp/obofoundry/sssom/transform/parser/SSSOMTransform.g4
@@ -137,7 +137,7 @@ CURIE     : PREFIX [a-zA-Z0-9_*-]+;
 
 PREFIX    : [a-zA-Z0-9_]+ ':';
 
-IRI       : '<' [a-zA-Z0-9_/:#=.-?&]+ '>';
+IRI       : '<' [a-zA-Z0-9_/:#=.?&-]+ '>';
 
 DOUBLE    : [0-9]+ '.' [0-9]+;
 