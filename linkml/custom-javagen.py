e5b38a235f77758c63e25d06ee3e50edbb58435b
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2026-03-27 23:21:17 +0000

    Directly generate "full" classes.
    We overhaul the custom code generation script and template so that it
    directly produces fully usable Java classes, without requiring the use
    of Lombok annotations.

--- linkml/custom-javagen.py
+++ linkml/custom-javagen.py
@@ -7,38 +7,40 @@ from linkml.utils.generator import shared_arguments
 from jinja2 import Template
 from jsonasobj2 import as_dict
 
+custom_types = {
+    "date": "LocalDate",
+    "mapping_cardinality_enum": "MappingCardinality",
+    "entity_type_enum": "EntityType",
+    "predicate_modifier_enum": "PredicateModifier",
+    "sssom_version_enum": "Version",
+    "NonRelativeURI": "String",
+}
+
 class_template = """\
 package org.incenp.obofoundry.sssom.model;
 
 import java.util.List;
 import java.util.ArrayList;
 import java.time.LocalDate;
-{% if cls.name == 'MappingSet' or cls.name == 'Mapping' -%}
 import java.util.Map;
 import java.util.HashMap;
-{% endif -%}
 {% if cls.name == 'Mapping' -%}
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.Collections;
-{% endif -%}
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-{% if gen.get_constrained_slots(cls) -%}
-import lombok.Setter;
+
+{% else %}
 {% endif -%}
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder(toBuilder = true)
-@Data
-@EqualsAndHashCode(callSuper=false)
+/**
+ * Represents a <code>{{ cls.source_class.name }}</code> object.
+ * <p>
+ * Automatically generated from the SSSOM LinkML schema.
+ */
 public class {{ cls.name }} {% if cls.is_a -%} extends {{ cls.is_a }} {%- endif %} {
+
+{#- Fields declarations. #}
 {%- for f in cls.fields %}
     {%- if f.source_slot.name != f.name %}
     @JsonProperty("{{ f.source_slot.name }}")
@@ -50,7 +52,7 @@ public class {{ cls.name }} {% if cls.is_a -%} extends {{ cls.is_a }} {%- endif 
     @Propagatable
     {%- endif %}
     {%- if f.source_slot.slot_uri %}
-    @SlotURI("{{ gen.expand_curie(f.source_slot.slot_uri) }}")
+    @SlotURI("{{ f.slot_uri }}")
     {%- endif %}
     {%- if f.source_slot.range == 'NonRelativeURI' %}
     @URI
@@ -58,27 +60,77 @@ public class {{ cls.name }} {% if cls.is_a -%} extends {{ cls.is_a }} {%- endif 
     {%- if gen.get_added_in_version(f.source_slot.name, cls.source_class.name) %}
     @Versionable(addedIn = {{ gen.get_added_in_version(f.source_slot.name, cls.source_class.name) }})
     {%- endif %}
-    {%- if gen.is_slot_constrained(f) %}
-    @Setter(AccessLevel.NONE)
-    {%- endif %}
-    private {% if f.source_slot.range == 'date' %}LocalDate
-            {%- elif f.source_slot.range == 'mapping_cardinality_enum' %}MappingCardinality
-            {%- elif f.source_slot.range == 'entity_type_enum' %}EntityType
-            {%- elif f.source_slot.range == 'predicate_modifier_enum' %}PredicateModifier
-            {%- elif f.source_slot.range == 'sssom_version_enum' %}Version
-            {%- elif f.source_slot.name == 'curie_map' %}Map<String,String>
-            {%- else %}{{ f.range }}{% endif %} {{ f.name }};
+    private {{ gen.get_range(f) }} {{ f.name }};
 {% endfor -%}
-{%- for f in gen.get_constrained_slots(cls) %}
+
+{#- Custom extension field. #}
+    private Map<String,ExtensionValue> extensions;
+
+{#- Constructors. #}
+
     /**
-     * Sets the {{ f.source_slot.name }} field to a new value.
-     *
-     * @param value The new {{ f.source_slot.name }} value to set.
-     * @throws IllegalArgumentException If the value is outside of the valid
-     *                                  range.
+     * Creates a new empty instance.
      */
-    public void set{{ f.name[0].upper() }}{{ f.name[1:] }}({{ f.range }} value) {
-        {%- if f.source_slot.maximum_value %}
+    public {{ cls.name }}() {
+    }
+
+    /**
+     * Creates a new instance from the specified values.
+     */
+    protected {{ cls.name }}({% for f in cls.fields %}final {{ gen.get_range(f) }} {{ f.name }},
+            {% endfor %}final Map<String,ExtensionValue> extensions) {
+{%- for f in cls.fields %}
+        this.{{ f.name }} = {{ f.name }};{% endfor %}
+        this.extensions = extensions;
+    }
+
+{#- Accessors. #}
+{% for f in cls.fields %}
+    /**
+     * Gets the value of the <code>{{ f.source_slot.name }}</code> slot.
+     */
+    public {{ gen.get_range(f) }} {{ gen.get_read_accessor_name(f.source_slot) }}() {
+        return this.{{ f.name }};
+    }
+
+{%- if f.source_slot.name == "curie_map" %}
+
+    /**
+     * Gets the prefix map, optionally initializing the map if needed.
+     *
+     * @param set If {@code true}, the underlying field will be initialized to
+     *            an empty map if it happens to be {@code null}.
+     * @return The prefix map.
+     */
+    public Map<String,String> getCurieMap(boolean set) {
+        if ( curieMap == null && set ) {
+            curieMap = new HashMap<>();
+        }
+        return curieMap;
+    }
+{%- elif f.source_slot.multivalued %}
+
+    /**
+     * Gets the list of <code>{{ f.source_slot.name }}</code> values, optionally
+     * initializing the list if needed.
+     *
+     * @param set If {@code true}, the underlying field will be initialized to
+     *            an empty list if it happens to be {@code null}.
+     * @return The list of {{ f.source_slot.name }} values.
+     */
+    public {{ gen.get_range(f) }} {{ gen.get_read_accessor_name(f.source_slot) }}(boolean set) {
+        if ( {{ f.name }} == null && set ) {
+            {{ f.name }} = new ArrayList<>();
+        }
+        return {{ f.name }};
+    }
+{%- endif %}
+
+    /**
+     * Sets the value of the <code>{{ f.source_slot.name }}</code> slot.
+     */
+    public void {{ gen.get_write_accessor_name(f.source_slot) }}(final {{ gen.get_range(f) }} value) {
+        {%- if f.source_slot.maximum_value is not none %}
         if ( value > {{ f.source_slot.maximum_value }} ) {
             throw new IllegalArgumentException("Invalid value for {{ f.source_slot.name }}");
         }
@@ -88,29 +140,17 @@ public class {{ cls.name }} {% if cls.is_a -%} extends {{ cls.is_a }} {%- endif 
             throw new IllegalArgumentException("Invalid value for {{ f.source_slot.name }}");
         }
         {%- endif %}
-        {{ f.name }} = value;
+        this.{{ f.name }} = value;
     }
 {% endfor -%}
-{%- for f in cls.fields %}
-{%- if f.source_slot.multivalued and f.range == 'List<String>' %}
+
+{# Additional accessors for the extension slot. #}
     /**
-     * Gets the list of {{ f.source_slot.name }} values, optionally
-     * initializing the list if needed.
-     *
-     * @param set If {@code true}, the underlying field will be initialized to
-     *            an empty list if it happens to be {@code null}.
-     * @return The list of {{ f.source_slot.name }} values.
+     * Gets the map of extension values.
      */
-    public {{ f.range }} get{{ f.name[0].upper() }}{{ f.name[1:] }}(boolean set) {
-        if ( {{ f.name }} == null && set ) {
-            {{ f.name }} = new ArrayList<>();
-        }
-        return {{ f.name }};
+    public Map<String,ExtensionValue> getExtensions() {
+        return extensions;
     }
-{% endif -%}
-{% endfor -%}
-{%- if cls.name == 'MappingSet' or cls.name == 'Mapping' %}
-    private Map<String,ExtensionValue> extensions;
 
     /**
      * Gets the map of extension values, optionally initializing the map if
@@ -126,53 +166,16 @@ public class {{ cls.name }} {% if cls.is_a -%} extends {{ cls.is_a }} {%- endif 
         }
         return extensions;
     }
-{% endif -%}
-{%- if cls.name == 'MappingSet' %}
-    /**
-     * Gets the prefix map, optionally initializing the map if needed.
-     *
-     * @param set If {@code true}, the underlying field will be initialized to
-     *            an empty map if it happens to be {@code null}.
-     * @return The prefix map.
-     */
-    public Map<String,String> getCurieMap(boolean set) {
-        if ( curieMap == null && set ) {
-            curieMap = new HashMap<>();
-        }
-        return curieMap;
-    }
 
     /**
-     * Gets the list of mappings, optionally initializing the underlying field
-     * to an empty list if needed.
-     *
-     * @param set If {@code true}, the underlying field will be initialized to
-     *            an empty list if it happens to be {@code null}.
-     * @return The list of mappings.
+     * Sets the map of extension values.
      */
-    public List<Mapping> getMappings(boolean set) {
-        if ( mappings == null && set ) {
-            mappings = new ArrayList<>();
-        }
-        return mappings;
+    public void setExtensions(final Map<String,ExtensionValue> value) {
+        this.extensions = value;
     }
 
-    /**
-     * Gets the list of extension definitions, optionally initializing the
-     * underlying field to an empty list if needed.
-     *
-     * @param set If {@code true}, the underlying field will be initialized to
-     *            an empty list if it happens to be {@code null}.
-     * @return The list of extension definitions.
-     */
-    public List<ExtensionDefinition> getExtensionDefinitions(boolean set) {
-        if ( extensionDefinitions == null && set ) {
-            extensionDefinitions = new ArrayList<>();
-        }
-        return extensionDefinitions;
-    }
-{% endif -%}
-{%- if cls.name == 'Mapping' %}
+{#- #}
+{% if cls.name == 'Mapping' %}
     /**
      * Indicates whether this mapping represents a "missing" mapping.
      * <p>
@@ -252,6 +255,25 @@ public class {{ cls.name }} {% if cls.is_a -%} extends {{ cls.is_a }} {%- endif 
     }
 
     /**
+     * Creates an inverted version of this mapping if possible.
+     * <p>
+     * Inversion is possible if the predicate is one of the known "common"
+     * predicates and is invertible. To invert a mapping with an arbitrary
+     * predicate, use {@link #invert(String)}.
+     *
+     * @return A new mapping that is the inverse of this one, or {@code null}
+     *         if inversion is not possible.
+     */
+    public Mapping invert() {
+        CommonPredicate predicate = CommonPredicate.fromString(predicateId);
+        if ( predicate == null || !predicate.isInvertible() ) {
+            return null;
+        }
+
+        return invert(predicate.getInverse());
+    }
+
+    /**
      * Creates a canonical S-expression representing this mapping.
      *
      * @return A String uniquely representing this mapping, as a canonical S-expression.
@@ -305,25 +327,6 @@ public class {{ cls.name }} {% if cls.is_a -%} extends {{ cls.is_a }} {%- endif 
     }
 
     /**
-     * Creates an inverted version of this mapping if possible.
-     * <p>
-     * Inversion is possible if the predicate is one of the known "common"
-     * predicates and is invertible. To invert a mapping with an arbitrary
-     * predicate, use {@link #invert(String)}.
-     *
-     * @return A new mapping that is the inverse of this one, or {@code null}
-     *         if inversion is not possible.
-     */
-    public Mapping invert() {
-        CommonPredicate predicate = CommonPredicate.fromString(predicateId);
-        if ( predicate == null || !predicate.isInvertible() ) {
-            return null;
-        }
-
-        return invert(predicate.getInverse());
-    }
-
-    /**
      * @deprecated Use {@code #getSimilarityScore()} instead.
      */
     @Deprecated
@@ -355,9 +358,134 @@ public class {{ cls.name }} {% if cls.is_a -%} extends {{ cls.is_a }} {%- endif 
         setSimilarityMeasure(value);
     }
 {% endif -%}
+
+{#- The overridden methods from Object. #}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{{ cls.name }}(");
+{%- for f in cls.fields %}
+{%-   if f.range in ["int", "float", "double", "boolean"] %}
+        sb.append("{{ gen.get_slot_actual_name(f.source_slot) }}=");
+        sb.append(this.{{ f.name }});
+        sb.append(",");
+{%-   else %}
+ {%-  endif %}
+        if ( this.{{ f.name }} != null ) {
+            sb.append("{{ gen.get_slot_actual_name(f.source_slot) }}=");
+            sb.append(this.{{ f.name }});
+            sb.append(",");
+        }
+{%- endfor %}
+        if ( extensions != null && !extensions.isEmpty() ) {
+            sb.append("extensions={");
+            for ( Map.Entry<String, ExtensionValue> entry : extensions.entrySet() ) {
+                sb.append(entry.getKey());
+                sb.append("=");
+                sb.append(entry.getValue());
+                sb.append(",");
+            }
+            sb.append("}");
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if ( o == this ) return true;
+        if ( !(o instanceof {{ cls.name }}) ) return false;
+        final {{ cls.name }} other = ({{ cls.name }}) o;
+        if ( !other.canEqual((Object) this)) return false;
+{#- No need to call the parent's method if we are directly derived from Object. #}
+{%- if cls.is_a %}
+        if ( !super.equals(o) ) return false;
+{% endif %}
+{#- Now compare every slots. #}
+{%- for f in cls.fields %}
+{%-   if f.range in ["int", "float", "double", "boolean"] %}
+        if ( this.{{ f.name }} != other.{{ f.name }} ) return false;
+{%-   else %}
+        if ( this.{{ f.name }} == null ? other.{{ f.name }} != null : !this.{{ f.name }}.equals(other.{{ f.name }})) return false;
+ {%-  endif %}
+{%- endfor %}
+        if ( this.extensions == null ? other.extensions != null : !this.extensions.equals(other.extensions)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof {{ cls.name }};
+    }
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = {% if cls.is_a %}super.hashCode(){% else %}1{% endif %};
+{%- for f in cls.fields %}
+{%-   if f.range == "int" %}
+        result = result * PRIME + this.{{ f.name }};
+{%-   elif f.range == "float" %}
+        result = result * PRIME + Float.floatToIntBits(this.{{ f.name }});
+{%-   elif f.range == "double" %}
+        final long ${{ f.name }} = Double.doubleToLongBits(this.{{ f.name }});
+        result = result * PRIME + (int) (${{ f.name }} >>> 32 ^ ${{ f.name }});
+{%-   elif f.range == "boolean" %}
+        result = result * PRIME + (this.{{ f.name }} ? 79 : 97);
+{%-   else %}
+        result = result * PRIME + (this.{{ f.name }} == null ? 43 : this.{{ f.name }}.hashCode());
+{%-   endif %}
+{%- endfor %}
+        result = result * PRIME + (this.extensions == null ? 43 : this.extensions.hashCode());
+        return result;
+    }
+
+{#- Builder object. #}
+
+    public static class {{ cls.name }}Builder {
+{%- for f in cls.fields %}
+        private {{ gen.get_range(f) }} {{ f.name }};{% endfor -%}
+{##}
+        private Map<String,ExtensionValue> extensions;
+
+        {{ cls.name }}Builder() {
+        }
+{##}
+{%- for f in cls.fields %}
+        public {{ cls.name }}.{{ cls.name }}Builder {{ f.name }}(final {{ gen.get_range(f) }} {{ f.name }}) {
+            this.{{ f.name }} = {{ f.name }};
+            return this;
+        }
+{% endfor -%}
+{##}
+        public {{ cls.name }}.{{ cls.name}}Builder extensions(final Map<String,ExtensionValue> extensions) {
+            this.extensions = extensions;
+            return this;
+        }
+
+        public {{ cls.name }} build() {
+            return new {{ cls.name }}({% for f in cls.fields %}this.{{ f.name }},
+                {% endfor %}this.extensions);
+        }
+
+        public String toString() {
+            return "{{ cls.name }}.{{ cls.name }}Builder({% for f in cls.fields %}{{ f.name }}=" + this.{{ f.name }}
+                + ", {% endfor %}extensions=" + this.extensions + ")";
+        }
+    }
+
+    public static {{ cls.name }}.{{ cls.name }}Builder builder() {
+        return new {{ cls.name }}.{{ cls.name}}Builder();
+    }
+
+    public {{ cls.name }}.{{ cls.name }}Builder toBuilder() {
+        return new {{ cls.name }}.{{ cls.name }}Builder(){% for f in cls.fields %}
+            .{{ f.name }}(this.{{ f.name }}){% endfor %}
+            .extensions(this.extensions);
+    }
 }
 
 """
+
 
 class CustomJavaGenerator(JavaGenerator):
     """
@@ -408,34 +536,31 @@ class CustomJavaGenerator(JavaGenerator):
             return f"Version.SSSOM_{major}_{minor}"
         return None
 
-    def expand_curie(self, curie):
-        return self.schemaview.expand_curie(curie)
-
-    def is_slot_constrained(self, slot):
-        """Check if the slot has special constraints.
-
-        :param slot: the slot to check for constraints
-        """
-
-        return slot.source_slot.minimum_value is not None or slot.source_slot.maximum_value is not None
-
-    def get_constrained_slots(self, cls):
-        """Get all slots in a class that have special constraints.
-
-        :param cls: the class to query for constrained slots
-        """
-
-        return [f for f in cls.fields if self.is_slot_constrained(f)];
-
     def get_slot_suffixes(self, cls, prefix):
         """Get the suffixes of all slots that start with the given prefix.
 
         :param cls: the class to query for slot suffixes.
         :param prefix: the prefix to look for in slot names.
         """
-        
+
         n = len(prefix)
         return [f.name[n:] for f in cls.fields if f.name.startswith(prefix)]
+
+    def get_range(self, field):
+        """Get the real range for a given field.
+
+        :param field: The field object produced by the code generator.
+        """
+        custom = custom_types.get(field.source_slot.range, None)
+        if custom:
+            if field.source_slot.multivalued:
+                return f"List<{custom}>"
+            else:
+                return custom
+        elif field.source_slot.name == "curie_map":
+            return "Map<String,String>"
+        else:
+            return field.range
 
 
 @click.argument("yamlfile", type=click.Path(exists=True, dir_okay=False))
